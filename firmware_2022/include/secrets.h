a5558c54ce5371051340492ed002880ed35c6166
Johannes Rebling <johannesrebling@metergroup.com>
2022-02-17 18:53:46 +0100

    - basic stuff working but it aint pretty

---
+++ firmware_2022/include/secrets.h
@@ -0,0 +1,8 @@
+#define SECRET_SSID "Kassiopeia"                                // replace MySSID with your WiFi network name
+#define SECRET_PASS "SternbilddesNordhimmels"                            // replace MyPassword with your WiFi password
+
+const char* ssid = "Kassiopeia";
+const char* password = "SternbilddesNordhimmels";
+
+const char* WIFI_SSID = "Kassiopeia";
+const char* WIFI_PWD = "SternbilddesNordhimmels";
--- firmware_2022/src/main.cpp
+++ firmware_2022/src/main.cpp
@@ -1,46 +1,72 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <U8g2lib.h>
 
-#include <U8g2lib.h> 
-#include <Wire.h>
+#include "SHT2x.h"
+#include "secrets.h"
 
-uint16_t line = 0;
 char lineBuffer[20];
 
-
-
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
+SHT2x sht;
 
 void setup(void) {
+
+  Serial.begin(115200);
+
+  Serial.print("u8g2\n");
   u8g2.begin();
   u8g2.enableUTF8Print();
+
+  Serial.print("sht\n");
+  sht.begin();
+  sht.getStatus();
+
+  // u8g2.setFont(u8g2_font_streamline_internet_network_t);
+  while (!sht.isConnected()) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.print("\n");
 }
 
-uint32_t i = 0;
+float temp = 0.00;
+float humid = 0.00;
 
 void loop(void) {
+
+  if (sht.isConnected()) {
+    sht.read();
+    sht.getError();
+    sht.getStatus();
+    humid = sht.getHumidity();
+    temp = sht.getTemperature();
+    Serial.print("humid ");
+    Serial.print(humid);
+    Serial.print(" temp ");
+    Serial.print(temp);
+    Serial.print("\n");
+  } else {
+    temp = -99.9;
+    humid = -99.9;
+  }
+
   u8g2.clearBuffer();
   u8g2.setFont(u8g2_font_smart_patrol_nbp_tf);
-
-  snprintf(lineBuffer, 20, "Humid: %03.2f %%", 22.33);
+  
+  snprintf(lineBuffer, 20, "Humid: %03.2f %%", humid);
   u8g2.drawStr(0, 20, lineBuffer);
 
-  snprintf(lineBuffer, 20, " Temp: %03.2f %%", 22.33);
+  snprintf(lineBuffer, 20, " Temp: %03.2f %%", temp);
   u8g2.drawStr(0, 40, lineBuffer);
 
   u8g2.setFont(u8g2_font_streamline_internet_network_t);
-  u8g2.setFontMode(1);  /* activate transparent font mode */
-  u8g2.setDrawColor(0);
-  u8g2.drawGlyph(100, 64, 0x31);	// WIFI not working
-  delay(500);
-  u8g2.sendBuffer();
-
-  u8g2.setDrawColor(2);
-  u8g2.drawGlyph(100, 64, 0x31);	// WIFI not working
-  delay(500);
+  u8g2.drawGlyph(100, 64, 0x31);  // WIFI not working
+  // u8g2.setFontMode(1); /* activate transparent font mode */
+  // u8g2.setDrawColor(0);
+  
   u8g2.sendBuffer();
 
 
-
-
-  // u8g2.drawGlyph(64, 64, 0x38);	// WIFI working 
+  delay(1000);
 }
